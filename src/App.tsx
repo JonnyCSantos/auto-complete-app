017b9f0e0c7967bc4912c37a2cf46597f6fcb885
jonnyCSantos <jonathandossantos.c@gmail.com>
2021-10-24 18:45:03 -0300

    feat: creating api data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import './App.css';
+import api from './Service/api'
 import Search from './components/Search/Search'
 
 function App() {
+  api.map(city => {
+    console.log("city", city.name)
+    return null
+  })
+  
   return (
     <div className="App">
       <div className="App__wrapper">
---
+++ src/Service/api.tsx
@@ -0,0 +1,68 @@
+const api = [
+    {
+        id: "0",
+        name: 'São Paulo',
+    },
+    {
+        id: "1",
+        name: 'Guarulhos',
+    },
+    {
+        id: "2",
+        name: 'Campinas',
+    },
+    {
+        id: "3",
+        name: 'São Bernardo do Campo',
+    },
+    {
+        id: "4",
+        name: 'São José dos Campos',
+    },
+    {
+        id: "5",
+        name: 'Santo André',
+    },
+    {
+        id: "6",
+        name: 'Ribeirão Preto',
+    },
+    {
+        id: "7",
+        name: 'Osasco',
+    },
+    {
+        id: "8",
+        name: 'Sorocaba',
+    },
+    {
+        id: "9",
+        name: 'Mauá',
+    },
+    {
+        id: "10",
+        name: 'São José do Rio Preto',
+    },
+    {
+        id: "11",
+        name: 'Mogi das Cruzes',
+    },
+    {
+        id: "12",
+        name: 'Santos',
+    },
+    {
+        id: "13",
+        name: 'Diadema',
+    },
+    {
+        id: "14",
+        name: 'Jundiaí',
+    },
+    {
+        id: "15",
+        name: 'Piracicaba',
+    },
+]
+
+export default api
